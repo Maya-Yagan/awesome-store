5ec4eb82d4ac3e9b190a3e8e93102c6911d71dd3
Maya-Yagan <maya2002yagan@gmail.com>
2024-06-20 01:55:19 +0300

    Initial commit

---
+++ awesome-store/src/layouts/MainLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="text-white bg-black q-py-md">
+      <q-toolbar >
+        <q-toolbar-title class="q-mx-sm">
+          Awesome Store
+        </q-toolbar-title>
+      <q-btn class="q-mx-sm text-capitalize" stretch flat label="Categories" />
+      <q-btn class="q-mx-sm text-capitalize" stretch flat label="My Account" />
+      <q-btn class="q-mx-sm text-capitalize" stretch flat label="Shopping Cart" />
+      <q-btn class="q-mx-sm text-capitalize" stretch flat label="Sign in/Sign up" />
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'MainLayout'
+});
+</script>
