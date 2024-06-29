15c9e0fd500ceade61e0842bbb0f725db36b592e
Maya-Yagan <maya2002yagan@gmail.com>
2024-06-29 23:28:14 +0300

    added props and emits

--- awesome-store/src/components/ProductsCarousel.vue
+++ awesome-store/src/components/ProductsCarousel.vue
@@ -14,23 +14,22 @@
       <div
         class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
       >
-      <product-card image="src/assets/p6.jpg"/>
-      <product-card image="src/assets/p1.jpg"/>
-      <product-card image="src/assets/p3.jpg"/>
-      <product-card image="src/assets/p4.jpg"/>
-      <product-card image="src/assets/p5.jpg"/>
-      <product-card image="src/assets/p6.jpg"/>
+      <product-card
+      v-for="product in props.products"
+      :key="product.id"
+      @response="(id) => emit('response', id)"
+      :product="product"/>
       </div>
     </q-carousel-slide>
     <q-carousel-slide :name="2" class="column no-wrap">
       <div
         class="row fit justify-start items-center q-gutter-xs q-col-gutter no-wrap"
       >
-      <product-card image="src/assets/p4.jpg"/>
-      <product-card image="src/assets/p4.jpg"/>
-      <product-card image="src/assets/p4.jpg"/>
-      <product-card image="src/assets/p4.jpg"/>
-      <product-card image="src/assets/p4.jpg"/>
+      <product-card
+      v-for="product in props.products"
+      :key="product.id"
+      @response="(id) => emit('response', id)"
+      :product="product"/>
       </div>
     </q-carousel-slide>
   </q-carousel>
@@ -39,5 +38,10 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import ProductCard from 'src/components/ProductCard.vue'
-   const slide = ref(1)
+import { Product } from 'src/types/Product';
+const slide = ref(1)
+const emit = defineEmits(['response'])
+const props = defineProps<{
+  products : Array<Product>
+}>()
 </script>
