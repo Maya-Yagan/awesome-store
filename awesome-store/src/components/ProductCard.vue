42d1810fcb61798ab94bbc564bb58469f8fe4fa4
Maya-Yagan <maya2002yagan@gmail.com>
2024-06-29 23:26:28 +0300

    props were added to the cards instead of the old static values

--- awesome-store/src/components/ProductCard.vue
+++ awesome-store/src/components/ProductCard.vue
@@ -1,27 +1,23 @@
 <template>
   <q-card flat bordered >
     <div class=" row items-center justify-center">
-      <q-img width="150px" height="190px" fit="contain"  :src="props.image" />
+      <q-img width="150px" height="190px" fit="contain"  :src="props.product.image" />
     </div>
       <q-card-section>
         <div class="row no-wrap items-center q-my-sm">
-          Awesome Casual Premium
+          {{ props.product.name }}
         </div>
         <div class="text-subtitle1 text-green-6">
-          <q-btn push class="text-capitalize fit bg-amber-1 text-grey-9">Add to cart: $ 59.99</q-btn>
+          <q-btn @click="emit('response', props.product.id)" push class="text-capitalize fit bg-amber-1 text-grey-9">Add to cart: $ {{ props.product.price }}</q-btn>
         </div>
       </q-card-section>
     </q-card>
 </template>
 
 <script setup lang="ts">
-
-import { defineProps } from 'vue';
-const props = defineProps({
-  image: {
-    type: String,
-    required: true,
-  },
-});
-console.log(props.image + 'hello')
+import { Product } from 'src/types/Product';
+const props = defineProps<{
+  product: Product
+}>();
+const emit = defineEmits(['response'])
 </script>
