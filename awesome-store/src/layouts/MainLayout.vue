91405a7deef50adf4abe4dfe92f66a706de9a1d3
Maya-Yagan <maya2002yagan@gmail.com>
2024-07-07 01:12:14 +0300

    added sign in/up functionalitiy in the nav bar

--- awesome-store/src/layouts/MainLayout.vue
+++ awesome-store/src/layouts/MainLayout.vue
@@ -5,10 +5,33 @@
         <q-toolbar-title to="/#" class="q-mx-sm">
           <q-btn class="text-capitalize text-h5 text-amber-1" stretch flat label="Awesome Store" to="/"/>
         </q-toolbar-title>
-      <q-btn class="q-mx-sm text-capitalize" stretch flat label="Categories" to="/categories" />
-      <q-btn class="q-mx-sm text-capitalize" stretch flat label="My Account" />
-      <q-btn class="q-mx-sm text-capitalize" stretch flat label="Sign in/Sign up" to="/signin"/>
-      <q-btn class="q-mx-sm text-capitalize" stretch flat stack icon="shopping_bag" to="/cart">
+      <q-btn
+      class="q-mx-sm text-capitalize"
+      stretch flat
+      label="Categories"
+      to="/categories" />
+      <q-btn
+      class="q-mx-sm text-capitalize"
+      stretch flat
+      label="My Account" />
+      <q-btn
+      v-if="!isSignedIn"
+      class="q-mx-sm text-capitalize"
+      stretch flat
+      label="Sign in/Sign up"
+      to="/signin"/>
+      <q-btn
+      v-else class="q-mx-sm text-capitalize"
+      stretch flat stack
+      :label="userName"
+      @click="signOut">
+        {{ SignOut }}
+      </q-btn>
+      <q-btn
+      class="q-mx-sm text-capitalize"
+      stretch flat stack
+      icon="shopping_bag"
+      to="/cart">
         <q-badge color="amber-6" floating class="text-grey-9 text-bold">{{ itemsCount }}</q-badge>
         ${{ totalPrice }}
       </q-btn>
@@ -24,9 +47,24 @@
 </template>
 
 <script setup lang="ts">
-  import { useCartStore } from 'src/stores/cart';
-  import { computed } from 'vue';
-  const store = useCartStore()
-  const itemsCount = computed(() => store.itemsCount())
-  const totalPrice = computed(() => store.getTotalPrice)
+import { useCartStore } from 'src/stores/cart';
+import { useFirestore } from 'src/stores/firestore';
+import { useUserStore } from 'src/stores/user';
+import { computed, ref, toRefs } from 'vue';
+import { useRouter } from 'vue-router'
+  const router = useRouter()
+  const cartStore = useCartStore()
+  const userStore = useUserStore()
+  const firestore = useFirestore()
+  const SignOut = ref('Sign Out')
+  const { isSignedIn } = toRefs(userStore);
+  const userName = ref(userStore.user?.firstName + ' ' + userStore.user?.lastName)
+  function signOut(){
+    firestore.logout().then(() => {
+      console.log('logged out')
+      router.push('/')
+    })
+  }
+  const itemsCount = computed(() => cartStore.itemsCount)
+  const totalPrice = computed(() => cartStore.getTotalPrice)
 </script>
