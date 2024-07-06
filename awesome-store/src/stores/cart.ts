3e831e9658ab029fdcae467471fe8ec1a99379f8
Maya-Yagan <maya2002yagan@gmail.com>
2024-07-07 01:19:13 +0300

    added firebase functionality

--- awesome-store/src/stores/cart.ts
+++ awesome-store/src/stores/cart.ts
@@ -1,14 +1,22 @@
 import { defineStore } from 'pinia';
 import { ref, computed } from 'vue';
 import { Product } from 'src/types/Product';
+import { auth } from 'src/boot/firebase';
+import { useFirestore } from './firestore';
 /*
     ref()s become state properties
     computed()s become getters
     function()s become actions
 */
 export const useCartStore = defineStore('cart', () => {
-  const cartItems = ref<Array<Product>>([]);
-  const totalPrice = ref(0);
+  const cartItems = ref<Product[]>([]);
+  const firestore = useFirestore()
+
+  const itemsCount = computed(() => {
+    return cartItems.value.reduce(
+      (accumulator, product) => accumulator + product.quantity, 0
+    )
+  })
 
   const getTotalPrice = computed(() => {
     return cartItems.value.reduce((accumulator, item) => {
@@ -16,35 +24,45 @@ export const useCartStore = defineStore('cart', () => {
     }, 0);
   });
 
-  function addToCart(product: Product) {
-    const existingProduct = cartItems.value.find(
-      (item) => item.id === product.id
-    );
-    if (existingProduct) {
-      existingProduct.quantity += 1;
-    } else {
-      product.quantity += 1;
-      cartItems.value.push(product);
+  async function addToCart(product: Product) {
+    const user = auth.currentUser
+    if(user && user.email){
+      const existingProduct = cartItems.value.find(
+        (item) => item.id === product.id
+      );
+      if(existingProduct){
+        existingProduct.quantity += 1;
+        await firestore.saveUserCartToFirestore(cartItems.value)
+      }
+      else{
+        const userCart = await firestore.getUserCartFromFirestore(user.email)
+        product.quantity += 1;
+        console.log(userCart)
+        userCart.push(product);
+        await firestore.saveUserCartToFirestore(userCart)
+        cartItems.value = userCart
+      }
     }
   }
 
-  function removeFromCart(product: Product) {
+  async function removeFromCart(product: Product) {
     const index = cartItems.value.findIndex((item) => item.id === product.id);
     cartItems.value.splice(index, 1);
+    try{
+      await firestore.saveUserCartToFirestore(cartItems.value)
+    }
+    catch(error){
+      console.error('Error updating FireStore', error)
+    }
   }
 
-  function removeOneItem(product: Product) {
+  async function removeOneItem(product: Product) {
     const index = cartItems.value.findIndex((item) => item.id === product.id);
-    if (cartItems.value[index].quantity > 1)
+    if (cartItems.value[index].quantity > 1){
       cartItems.value[index].quantity -= 1;
+      await firestore.saveUserCartToFirestore(cartItems.value)
+    }
     else removeFromCart(product);
-  }
-
-  function itemsCount(): number {
-    return cartItems.value.reduce(
-      (accumulator, product) => accumulator + product.quantity,
-      0
-    );
   }
 
   return {
@@ -53,7 +71,6 @@ export const useCartStore = defineStore('cart', () => {
     itemsCount,
     removeOneItem,
     cartItems,
-    totalPrice,
     getTotalPrice,
   };
 });
