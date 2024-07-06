bbc09a44e4e52428afe0ec54d94c232d00738645
Maya-Yagan <maya2002yagan@gmail.com>
2024-07-07 01:38:10 +0300

    firebase update

--- awesome-store/src/boot/firebase.ts
+++ awesome-store/src/boot/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,16 +9,12 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAlJie8lUNZFYcPdxJPBHrmd96U6kKUNUE',
-  authDomain: 'awesome-store-c4ea1.firebaseapp.com',
-  projectId: 'awesome-store-c4ea1',
-  storageBucket: 'awesome-store-c4ea1.appspot.com',
-  messagingSenderId: '628530138918',
-  appId: '1:628530138918:web:4b38862fe3915a39f2fbe7',
-  measurementId: 'G-VC2BT8NN6W'
+  //configs
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider();
 
